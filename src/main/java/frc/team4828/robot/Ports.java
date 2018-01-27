4916ece61d5172c0f9dcf17afdf9efdd802d504f
Jackson McClintock <jacksondm33@gmail.com>
2018-01-27 12:59:12 -0500

    Optimize gradle and code

---
+++ src/main/java/frc/team4828/robot/Ports.java
@@ -0,0 +1,12 @@
+package frc.team4828.robot;
+
+public class Ports {
+
+    public static final int FRONT_LEFT = 1;
+    public static final int FRONT_RIGHT = 2;
+    public static final int BACK_LEFT = 3;
+    public static final int BACK_RIGHT = 4;
+
+    public static final int JOYSTICK = 0;
+
+}
