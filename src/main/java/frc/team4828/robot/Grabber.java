e78cea9f50a32969561c63705aab44626e830bc0
Jackie Chen <cheezballz3.14@gmail.com>
2018-01-27 16:52:44 -0500

    Add Grabber class

---
+++ src/main/java/frc/team4828/robot/Grabber.java
@@ -0,0 +1,32 @@
+package frc.team4828.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Grabber {
+    TalonSRX left, right;
+
+    public Grabber(int leftPort, int rightPort) {
+        left = new TalonSRX(leftPort);
+        right = new TalonSRX(rightPort);
+    }
+
+    public void intake() {
+        left.set(ControlMode.PercentOutput, 1);
+        right.set(ControlMode.PercentOutput, -1);
+    }
+
+    public void outtake() {
+        left.set(ControlMode.PercentOutput, 0);
+        right.set(ControlMode.PercentOutput, 0);
+    }
+
+    public void stop() {
+        left.set(ControlMode.PercentOutput, -1);
+        right.set(ControlMode.PercentOutput, 1);
+    }
+    public void set(int spd) {
+        left.set(ControlMode.PercentOutput, spd);
+        right.set(ControlMode.PercentOutput, spd);
+    }
+}
