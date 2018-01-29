b7e3ae8825f8c38b234f997314ab36c03484e0f3
Jackie Chen <cheezballz3.14@gmail.com>
2018-01-29 18:39:05 -0500

    Add Drivetrain

--- src/main/java/frc/team4828/robot/DriveTrain.java
+++ src/main/java/frc/team4828/robot/DriveTrain.java
@@ -1,9 +1,29 @@
 package frc.team4828.robot;
 
 public class DriveTrain {
-
-    public DriveTrain() {
-
+    Gearbox left, right;
+    public DriveTrain(Gearbox l, Gearbox r) {
+        left = l;
+        right = r;
+    }
+    public double[] normalize(double[] in) {
+        double max = -10;
+        for(int i = 0; i < in.length; i++) {
+            if(in[i] > max) {
+                max = in[i];
+            }
+        }
+        max = Math.abs(max);
+        for(int i = 0; i < in.length; i++) {
+            in[i] /= max;
+        }
+        return in;
+    }
+    public void arcadeDrive(double x, double y, double twist) {
+        double[] drive = { y - x - twist, y + x + twist };
+        drive = normalize(drive);
+        left.drive(drive[0]);
+        right.drive(drive[1]);
     }
 
 }
