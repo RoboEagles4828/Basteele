90d603a969aefd0e6cedbc6ec1ef902fb7ea68b1
Jackson McClintock <jacksondm33@gmail.com>
2018-02-03 15:40:31 -0500

    Change closed loop control and add javadoc html files

--- src/main/java/frc/team4828/robot/Robot.java
+++ src/main/java/frc/team4828/robot/Robot.java
@@ -30,6 +30,7 @@ public class Robot extends IterativeRobot {
         motor3 = new TalonSRX(Ports.RIGHT_MOTORS[0]);
         motor4 = new TalonSRX(Ports.RIGHT_MOTORS[1]);
         comp = new Compressor(Ports.COMPRESSOR);
+        comp.setClosedLoopControl(true);
         sol1 = new DoubleSolenoid(Ports.LEFT_SOLENOID[0], Ports.LEFT_SOLENOID[1]);
         sol2 = new DoubleSolenoid(Ports.RIGHT_SOLENOID[0], Ports.RIGHT_SOLENOID[1]);
         switcher1 = new PneumaticSwitch(comp, sol1);
