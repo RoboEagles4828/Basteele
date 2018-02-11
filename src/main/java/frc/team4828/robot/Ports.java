29f75011c599fb8fa47984667fbd795b4f937a3c
Jackson McClintock <jacksondm33@gmail.com>
2018-02-11 16:39:09 -0500

    Change Ports

--- src/main/java/frc/team4828/robot/Ports.java
+++ src/main/java/frc/team4828/robot/Ports.java
@@ -7,16 +7,16 @@ public class Ports {
 
     public static final int COMPRESSOR = 5;
 
-    public static final int LIFT_MOTOR = 0;
-    public static final int ARM_MOTOR = 0;
+    public static final int LIFT_MOTOR = 4;
+    public static final int ARM_MOTOR = 5;
     public static final int LEFT_GRABBER = 6;
     public static final int RIGHT_GRABBER = 7;
 
-    public static final int LIFT_MIN = 0;
-    public static final int LIFT_MAX = 0;
-    public static final int ARM_MIN = 0;
-    public static final int ARM_MAX = 0;
-    public static final int SWITCHER = 0;
+    public static final int LIFT_MIN = 5;
+    public static final int LIFT_MAX = 6;
+    public static final int ARM_MIN = 7;
+    public static final int ARM_MAX = 8;
+    public static final int SWITCHER = 9;
 
     public static final int JOYSTICK = 0;
 
