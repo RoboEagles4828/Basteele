1bd29f387c3294769a65b610b482bc629d27f96c
Jackson McClintock <jacksondm33@gmail.com>
2018-02-17 13:47:19 -0500

    Format climber

--- src/main/java/frc/team4828/robot/Climber.java
+++ src/main/java/frc/team4828/robot/Climber.java
@@ -3,22 +3,37 @@ package frc.team4828.robot;
 import edu.wpi.first.wpilibj.Victor;
 
 public class Climber {
-    private Victor rightMotor,leftMotor;
-    double speed;
-    public Climber(int leftMotorPort, int rightMotorPort){
-        rightMotor = new Victor(rightMotorPort);
-        leftMotor = new Victor(leftMotorPort);
 
-}
-    public void climbUp(double speed){
-        rightMotor.set(speed);
+    private Victor leftMotor, rightMotor;
+
+    private static final double DEFAULT_SPEED = 0.5;
+
+    public Climber(int leftMotorPort, int rightMotorPort) {
+        leftMotor = new Victor(leftMotorPort);
+        rightMotor = new Victor(rightMotorPort);
+    }
+
+    public void up() {
+        leftMotor.set(DEFAULT_SPEED);
+        rightMotor.set(DEFAULT_SPEED);
+    }
+
+    public void up(double speed) {
         leftMotor.set(speed);
+        rightMotor.set(speed);
     }
-    public void climbDown(double speed){
-        rightMotor.set(-speed);
+
+    public void down() {
+        leftMotor.set(-DEFAULT_SPEED);
+        rightMotor.set(-DEFAULT_SPEED);
+    }
+
+    public void down(double speed) {
         leftMotor.set(-speed);
+        rightMotor.set(-speed);
     }
-    public void stopClimb(){
+
+    public void stop() {
         rightMotor.set(0);
         leftMotor.set(0);
     }
