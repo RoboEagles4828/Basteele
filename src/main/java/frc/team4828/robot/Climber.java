2e86b83bfba5811d4506e8c02cc0123eb001ebde
Jackie Chen <cheezballz3.14@gmail.com>
2018-03-16 21:48:40 -0400

    Javadoc the Climber

--- src/main/java/frc/team4828/robot/Climber.java
+++ src/main/java/frc/team4828/robot/Climber.java
@@ -7,6 +7,12 @@ public class Climber {
     private static final double DEFAULT_SPEED = 0.5;
     private Victor leftMotor, rightMotor;
 
+    /**
+     * Climber Code. Pretty self explanatory, up to go up, down to go down, stop to stop.
+     * Takes in 2 ports.
+     * @param leftMotorPort  Left Motor Port
+     * @param rightMotorPort Right Motor Port
+     */
     public Climber(int leftMotorPort, int rightMotorPort) {
         leftMotor = new Victor(leftMotorPort);
         rightMotor = new Victor(rightMotorPort);
