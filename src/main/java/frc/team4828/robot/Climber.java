14ccde4098572317f9990af7164e3d668c346cd4
Jackie Chen <cheezballz3.14@gmail.com>
2018-03-16 22:05:23 -0400

    Lift now goes full speed

--- src/main/java/frc/team4828/robot/Climber.java
+++ src/main/java/frc/team4828/robot/Climber.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.Victor;
 
 public class Climber {
 
-    private static final double DEFAULT_SPEED = 0.5;
+    private static final double DEFAULT_SPEED = 1;
     private Victor leftMotor, rightMotor;
 
     /**
@@ -13,6 +13,7 @@ public class Climber {
      * @param leftMotorPort  Left Motor Port
      * @param rightMotorPort Right Motor Port
      */
+
     public Climber(int leftMotorPort, int rightMotorPort) {
         leftMotor = new Victor(leftMotorPort);
         rightMotor = new Victor(rightMotorPort);
