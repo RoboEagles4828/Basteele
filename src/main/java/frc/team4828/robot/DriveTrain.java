e039d39066007d9489d99a634142cbaa8e07b690
Jackson McClintock <jacksondm33@gmail.com>
2018-03-27 18:17:24 -0400

    Move brakes to work after timeout

--- src/main/java/frc/team4828/robot/DriveTrain.java
+++ src/main/java/frc/team4828/robot/DriveTrain.java
@@ -199,13 +199,12 @@ public class DriveTrain {
             if (Math.abs(currentEnc) > MOVE_ENC_THRESH) {
                 speed = normalizeAbs(currentEnc, MOVE_RAMP_FACTOR, MOVE_MIN_SPEED, maxSpeed);
             } else {
-                brake();
                 break;
             }
 
             Timer.delay(MOVE_CHECK_DELAY);
         }
-
+        brake();
         debugEnc("End MOVE");
         debugNavx("End MOVE");
     }
@@ -232,13 +231,12 @@ public class DriveTrain {
                 currentAngle = normalizeAbs(currentAngle, TURN_FACTOR, TURN_MIN_SPEED, speed);
                 turn(currentAngle);
             } else {
-                brake();
                 break;
             }
 
             Timer.delay(TURN_CHECK_DELAY);
         }
-
+        brake();
         debugEnc("End TURN");
         debugNavx("End TURN");
     }
